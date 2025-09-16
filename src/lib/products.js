8ebc9b891763de2616e8f3677c836761a3a747ce
ngoyal195 <namangoyal195@gmail.com>
2025-09-16 11:49:15 +0530

    Create products.js

---
+++ src/lib/products.js
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    slug: 'travence-cabin-lite',
+    name: 'Travence Cabin Lite 20"',
+    price: 6999,
+    rating: 4.6,
+    image: '/images/cabin-lite.jpg',
+    weight: '3.6kg',
+    dims: '55 x 36 x 22 cm'
+  },
+  {
+    id: 2,
+    slug: 'travence-expander-24',
+    name: 'Travence Expander 24"',
+    price: 8999,
+    rating: 4.5,
+    image: '/images/expander-24.jpg',
+    weight: '4.2kg',
+    dims: '66 x 42 x 28 cm'
+  },
+  {
+    id: 3,
+    slug: 'travence-business-roller',
+    name: 'Travence Business Roller',
+    price: 7999,
+    rating: 4.7,
+    image: '/images/business-roller.jpg',
+    weight: '3.9kg',
+    dims: '58 x 38 x 24 cm'
+  }
+];
+
+export default products;
