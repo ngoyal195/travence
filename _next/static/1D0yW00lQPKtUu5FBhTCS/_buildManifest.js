71f2cac19a3120f6496019999168fa1a442b33b6
ngoyal195 <ngoyal195@users.noreply.github.com>
2025-09-18 08:39:26 +0000

    deploy: 76fd8ef18b53ab79ee18b742d40d24a1e7ae8895

--- _next/static/1D0yW00lQPKtUu5FBhTCS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ab355c1479fb5841.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],"/product/[slug]":["static/chunks/pages/product/[slug]-c40afd4e2106fdb4.js"],sortedPages:["/","/_app","/_error","/product/[slug]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
