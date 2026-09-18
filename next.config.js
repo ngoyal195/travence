44f2ba0abc7eb9d8ac1e799716169e9b5135f68d
ngoyal195 <namangoyal195@gmail.com>
2026-09-18 14:17:56 +0530

    Update next.config.js for root domain settings
    Removed basePath and assetPrefix for root domain deployment.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  // Since your site is now served from the root domain, remove basePath and assetPrefix
-  basePath: '',
-  assetPrefix: './',
   images: { unoptimized: true }, // important for static export
 };
 
