ebcaea1bc753891e8bd1021614f6cc8ef5efa2ae
ngoyal195 <namangoyal195@gmail.com>
2026-09-18 16:22:33 +0530

    Add components directory to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
 
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}"
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
 
   theme: {
